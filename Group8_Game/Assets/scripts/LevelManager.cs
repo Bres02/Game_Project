37176071d4f8535bd68dfbf7e7b5ff0861d0af5d
jlindman <Joshua.Lindman@gmail.com>
2023-12-05 13:46:45 -0500

    Menu controls and basic menues

---
+++ Group8_Game/Assets/scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void StartInstructions()
+    {
+        SceneManager.LoadScene("InstructionScreen");
+    }
+
+    public void LoadLevelTwo()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    public void LoadLevelThree()
+    {
+        SceneManager.LoadScene("Level_3");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("LoseScreen");
+    }
+
+    public void LoadVictory()
+    {
+        SceneManager.LoadScene("WinScreen");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScreen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
